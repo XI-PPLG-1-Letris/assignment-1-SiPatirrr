3d2da2d6129952c365cf5b31428c801ea26d40bc
Fathir <Hibatullahfathir87@gmail.com>
2025-10-05 09:07:14 +0700

    done pak

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let b = 3;
 
 // Operasi Aritmetika
 const tambah = (a,b) => a + b;
-console.log(`Penjumlahan ${a} + ${b} = ${a - b}`);
+console.log(`Penjumlahan ${a} + ${b} = ${a + b}`);
 
 const kurang = (a,b) => a - b;
 console.log(`Pengurangan ${a} - ${b} = ${a - b}`);
@@ -13,8 +13,8 @@ const kali = (a,b) => a * b;
 console.log(`Perkalian ${a} * ${b} = ${a * b}`);
 
 const bagi = (a,b) => a / b;
-console.log(`Pembagian ${a} / ${b} = ${a + b}`);
+console.log(`Pembagian ${a} / ${b} = ${a / b}`);
 
 const modulus = (a,b) => a % b;
-console.log(`Sisa ${a} % ${b} = ${a + b}`);
+console.log(`Sisa ${a} % ${b} = ${a % b}`);
 
