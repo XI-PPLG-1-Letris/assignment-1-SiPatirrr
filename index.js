d6ea4e325211eac314127ddd0b2e1542b3437dcb
Fathir <Hibatullahfathir87@gmail.com>
2025-10-05 09:05:55 +0700

    Done Pak

---
+++ index.js
@@ -0,0 +1,20 @@
+// Variable
+let a = 20;
+let b = 3;
+
+// Operasi Aritmetika
+const tambah = (a,b) => a + b;
+console.log(`Penjumlahan ${a} + ${b} = ${a - b}`);
+
+const kurang = (a,b) => a - b;
+console.log(`Pengurangan ${a} - ${b} = ${a - b}`);
+
+const kali = (a,b) => a * b;
+console.log(`Perkalian ${a} * ${b} = ${a * b}`);
+
+const bagi = (a,b) => a / b;
+console.log(`Pembagian ${a} / ${b} = ${a + b}`);
+
+const modulus = (a,b) => a % b;
+console.log(`Sisa ${a} % ${b} = ${a + b}`);
+
